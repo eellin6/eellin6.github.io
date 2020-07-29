ce3cdde8e2ddb3ac6e2b2e38339d7a3530c815bf
Ubuntu <ubuntu@ip-172-31-63-104.ec2.internal>
2020-07-29 21:09:20 +0000

    studies

---
+++ studies/hoisting.js
@@ -0,0 +1,18 @@
+/**
+ * Hoisting:
+ *
+ * 0. An interesting attribute of JavaScript interpreter that moves the function and variable declarations 
+ *    to the top of the current referenced scope. JavaScript's default behavior of moving declarations to the top.
+ *    
+ * 1. Functions declarations are also hoisted, but these go to the very top,
+ *    so will sit above all of the variable declarations.
+ * 
+ * 3. JS Interpreter: 
+ */
+
+
+
+// hoisting
+// How are variables hoisted in JavaScript?
+// How are function declarations hoisted? How are function expressions hoisted?
+// What is the JS Interpreter 
